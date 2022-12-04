d3aaf8c6c63abc0aa77076de9cd60697545e7296
Joancc04 <joancolillas2004@gmail.com>
2022-12-04 23:20:42 +0100

    bugs arreglats, executable creat

--- projecte.py
+++ projecte.py
@@ -7,31 +7,31 @@ class Hotel():
         self.carrer = carrer
         self.numero = numero
         self.codi_barri = codi_barri
-        self.codi_postal = codi_postal 
+        self.codi_postal = codi_postal
         self.telefon = telefon
         self.latitud = latitud
         self.longitud = longitud
         self.estrelles = estrelles
 
-        # == Comprovant variables tipus int == 
+        # == Comprovant variables tipus int ==
         vars_int = [[self.numero, "numero"], [self.codi_barri, "codi_barri"], [self.estrelles, "estrelles"]]
         for i in vars_int:
             if not isinstance(i[0], int):
                 raise TypeError (f"{i[1]} ha de ser un valor enter positiu")
-        
-        # == Comprovant variables tipus float == 
+
+        # == Comprovant variables tipus float ==
         vars_float = [[self.latitud, "latitud"], [self.longitud, "longitud"]]
         for i in vars_float:
             if not isinstance(i[0], float):
                 raise TypeError (f"{i[1]} ha de ser un valor real")
 
-        # == Comprovant estrelles entre 1 i 5 == 
+        # == Comprovant estrelles entre 1 i 5 ==
         if self.estrelles not in range(1,6):
             raise ValueError ("estrelles ha de ser un valor entre 1 i 5")
-    
+
     def __str__(self):
         return f"{self.nom} ({self.codi_hotel}) {self.carrer},{self.numero} {self.codi_postal} (barri: {self.codi_barri}) {self.telefon} ({self.latitud},{self.longitud}) {self.estrelles} estrelles"
-        
+
     def __gt__(self, altre_hotel):
         return self.estrelles > altre_hotel.estrelles
 
@@ -56,13 +56,17 @@ def codi_in_llista_hotels(llista_hotels, codi_hotel):
 def importar_hotels(fitxer,IFS):
     llista_hotels=[]
     try:
-        with open(f'{fitxer}', 'r', encoding='utf-8') as file:
-            for linia in file[1:]:
-                aux, carrer, numero, codi_barri, codi_postal, telefon, latitud, longitud, estrelles = linia[:-2].split(IFS)
-                nom, codi_hotel = aux.split(' - ')
+        with open(fitxer, 'r') as file:
+            for linia in file:
+                linia = file.readline()
+                aux, carrer, numero, codi_barri, codi_postal, telefon, latitud, longitud, estrelles = linia.split(IFS)
+                llista= aux.split(" - ")
+                nom = llista[0]
+                codi_hotel = llista[-1]
                 if not codi_in_llista_hotels(llista_hotels, codi_hotel):
                     llista_hotels.append(Hotel(nom, codi_hotel, carrer, int(numero), int(codi_barri), codi_postal, telefon, float(latitud)/1000000, float(longitud)/1000000, int(estrelles)))
-    except FileNotFoundError as a:
+        return llista_hotels
+    except FileNotFoundError:
         print("fitxer no trobat")
 
 class Barri():
@@ -71,14 +75,29 @@ class Barri():
         self.codi_districte = codi_districte
         if not isinstance(self.codi_districte, int) and self.codi_districte < 0:
             raise TypeError ("codi_districte ha de ser un valor enter positiu")
-    
+
     def __str__(self):
         return f"{self.nom} (districte: {self.codi_districte})"
+
+llista = importar_hotels("hotels.csv", ";")
+for i in llista:
+    print(i.nom, i.codi_hotel,i.estrelles)
+
+class Districte():
+    def __init__(self, nom, extensio, poblacio, llista_barris):
+        self.nom = nom
+        self.extensio = extensio
+        self.poblacio = poblacio
+        self.llista_barris = llista_barris
+        if not isinstance(poblacio, int) and poblacio < 0:
+            raise TypeError ("poblacio ha de ser un valor enter positiu")
+        if not isinstance(extensio, float) and extensio < 0:
+            raise TypeError ("extensio ha de ser un valor real positiu")
+        
+    def __str__(self):
+        return f"{self.nom} ({self.extensio} kms2, {self.poblacio} habitants) barris: {self.llista_barris}"
         
 
-
-
-        
 
 
 #      nom  , codi_hotel, carrer, numero, codi_barri,codi_postal,telf,latitud,longitud,estrelles
--- proves.py
+++ proves.py
@@ -1,33 +1,38 @@
-a = 6
-b = "hola"
-c = "ñalskjd"
+# a = 6
+# b = "hola"
+# c = "ñalskjd"
 
-class Hotel():
-    def __init__(self, codi_hotel):
-        self.codi_hotel = codi_hotel
+# class Hotel():
+#     def __init__(self, codi_hotel):
+#         self.codi_hotel = codi_hotel
 
-p1 = Hotel(1234)
-p2 = Hotel(2345)
-p3 = Hotel(3456)
-llista=[Hotel(1234),Hotel(2345),Hotel(3456)]
-#llista=[[1234,1235,1236],[2345,456456,2323],[234234,3234,2342]]
+# p1 = Hotel(1234)
+# p2 = Hotel(2345)
+# p3 = Hotel(3456)
+# llista=[Hotel(1234),Hotel(2345),Hotel(3456)]
+# #llista=[[1234,1235,1236],[2345,456456,2323],[234234,3234,2342]]
 
-def codi_in_llista_hotels(llista_hotels, codi_hotel):
-    for i in llista_hotels:
-        if codi_hotel == i.codi_hotel:
-            return True
-    else:
-        return False
+# def codi_in_llista_hotels(llista_hotels, codi_hotel):
+#     for i in llista_hotels:
+#         if codi_hotel == i.codi_hotel:
+#             return True
+#     else:
+#         return False
+# string = "Amrey Sant Pau - HB-004046"
+# list1=[0]*2
+# list1[0], list1[1] = string.split(" - ")
+# print(list1)
 
-
-
+#==============================================================
 # print(codi_in_llista_hotels(llista, 1234))
-paraules = "hola que tal"
-
-llista=paraules[:-2].split(" ")
-print(llista)
-
-
+# paraules = "hola que tal"
+# llista=paraules[:-2].split(" ")
+# print(llista)
 # def hola():
 #     return f"hola que tal"
 # print(hola())
+#==============================================================
+
+a = "hola"
+try:
+    print("hola")
