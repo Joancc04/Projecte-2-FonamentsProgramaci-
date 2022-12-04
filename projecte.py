a279e00a282432f82fd3be641cae09f5de1c5c6b
Joancc04 <joancolillas2004@gmail.com>
2022-12-04 19:58:07 +0100

    exercicis inicials fets

--- projecte.py
+++ projecte.py
@@ -1,3 +1,5 @@
+from math import *
+
 class Hotel():
     def __init__(self, nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitutd, longitud, estrelles):
         self.nom = nom
@@ -22,6 +24,7 @@ class Hotel():
         for i in vars_float:
             if not isinstance(i[0], float):
                 raise TypeError (f"{i[1]} ha de ser un valor real")
+
         # == Comprovant estrelles entre 1 i 5 == 
         if self.estrelles not in range(1,6):
             raise ValueError ("estrelles ha de ser un valor entre 1 i 5")
@@ -29,8 +32,43 @@ class Hotel():
     def __str__(self):
         return f"{self.nom} ({self.codi_hotel}) {self.carrer},{self.numero} {self.codi_postal} (barri: {self.codi_barri}) {self.telefon} ({self.latitud},{self.longitud}) {self.estrelles} estrelles"
         
+    def __gt__(self, altre_hotel):
+        return self.estrelles > altre_hotel.estrelles
+
+    def distancia(self, latitud, longitud):
+        vars_float=[[latitud,"latitud"],[longitud,"longitud"]]
+        RADI_TERRA = 6378.137
+        latitud *= pi/180
+        longitud *= pi/180
+        for i in vars_float:
+            if not isinstance(i[0], float):
+                raise TypeError (f"{i[1]} ha de ser un valor real")
+        return acos(sin(self.latitud) * sin(latitud) + cos(self.latitud) * cos(latitud) * cos(longitud - self.longitud)) * RADI_TERRA
+
+def codi_in_llista_hotels(llista_hotels, codi_hotel):
+    for i in llista_hotels:
+        if codi_hotel == i.codi_hotel:
+            return True
+    else:
+        return False
+
+#IFS = Input Field Separator
+def importar_hotels(fitxer,IFS):
+    llista_hotels=[]
+    try:
+        with open('fitxer', 'r', encoding='utf-8') as file:
+            for linia in file[1:]:
+                nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitutd, longitud, estrelles = linia[:-2].split(IFS)
+                llista_hotels.append(Hotel(nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitutd, longitud, estrelles))
+    except FileNotFoundError as a:
+        print("fitxer no trobat")
+
+
+
         
 
 
 #      nom  , codi_hotel, carrer, numero, codi_barri,codi_postal,telf,latitud,longitud,estrelles
-Hotel("Joan",      1,        "2",    12,        12,     12,     12,   12.5,      12.5,       12)
+# h1 = Hotel("Joan",      1,        "2",    12,        12,     12,     12,   12.5,      12.5,       4)
+# h2 = Hotel("Hotel H10 Itaca", "HB-004151", "Roma",    22, 9,     8015,     932265594,   41.381193,      2.145467,       4)
+# print(h2)
