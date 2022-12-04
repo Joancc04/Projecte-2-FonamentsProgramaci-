1696373167027c445e15c343cf28ec1b8f412fce
Joancc04 <joancolillas2004@gmail.com>
2022-12-04 20:18:29 +0100

    Fet fins exercici 4 perquè no sé com van els diccionaris

--- projecte.py
+++ projecte.py
@@ -1,7 +1,7 @@
 from math import *
 
 class Hotel():
-    def __init__(self, nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitutd, longitud, estrelles):
+    def __init__(self, nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitud, longitud, estrelles):
         self.nom = nom
         self.codi_hotel = codi_hotel
         self.carrer = carrer
@@ -9,7 +9,7 @@ class Hotel():
         self.codi_barri = codi_barri
         self.codi_postal = codi_postal 
         self.telefon = telefon
-        self.latitud = latitutd
+        self.latitud = latitud
         self.longitud = longitud
         self.estrelles = estrelles
 
@@ -56,12 +56,25 @@ def codi_in_llista_hotels(llista_hotels, codi_hotel):
 def importar_hotels(fitxer,IFS):
     llista_hotels=[]
     try:
-        with open('fitxer', 'r', encoding='utf-8') as file:
+        with open(f'{fitxer}', 'r', encoding='utf-8') as file:
             for linia in file[1:]:
-                nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitutd, longitud, estrelles = linia[:-2].split(IFS)
-                llista_hotels.append(Hotel(nom, codi_hotel, carrer, numero, codi_barri, codi_postal, telefon, latitutd, longitud, estrelles))
+                aux, carrer, numero, codi_barri, codi_postal, telefon, latitud, longitud, estrelles = linia[:-2].split(IFS)
+                nom, codi_hotel = aux.split(' - ')
+                if not codi_in_llista_hotels(llista_hotels, codi_hotel):
+                    llista_hotels.append(Hotel(nom, codi_hotel, carrer, int(numero), int(codi_barri), codi_postal, telefon, float(latitud)/1000000, float(longitud)/1000000, int(estrelles)))
     except FileNotFoundError as a:
         print("fitxer no trobat")
+
+class Barri():
+    def __init__(self, nom, codi_districte):
+        self.nom = nom
+        self.codi_districte = codi_districte
+        if not isinstance(self.codi_districte, int) and self.codi_districte < 0:
+            raise TypeError ("codi_districte ha de ser un valor enter positiu")
+    
+    def __str__(self):
+        return f"{self.nom} (districte: {self.codi_districte})"
+        
 
 
 
