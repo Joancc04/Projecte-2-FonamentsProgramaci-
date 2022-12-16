f4f1abd5b3be36d67ce5fd3164e6c9650edb0571
Joancc04 <joancolillas2004@gmail.com>
2022-12-16 13:34:44 +0100

    backup

--- proves_main.py
+++ proves_main.py
@@ -1,8 +1,9 @@
-try:
-    a = int(input())
-    if a < 0:
-        raise ValueError("Error: el nombre és més petit que 0")
-    elif a > 15:
-        raise ValueError("Erro: el nombre és més gran que 15")
-except ValueError as a:
-    print(a)
+# try:
+#     a = int(input())
+#     if a < 0:
+#         raise ValueError("Error: el nombre és més petit que 0")
+#     elif a > 15:
+#         raise ValueError("Erro: el nombre és més gran que 15")
+# except ValueError as a:
+#     print(a)
+
