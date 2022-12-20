81f4a2938fa9527671bda8843a14e61b321ad8b3
Joancc04 <joancolillas2004@gmail.com>
2022-12-20 13:08:43 +0100

    exercicis 1 i 2 acabats de locos

--- main.py
+++ main.py
@@ -15,33 +15,39 @@ def main():
     except Exception as e:
         print("Error processant els fitxers:", e)
     else:
-        omplir_llista_barris(barris, districtes)
-        op = "a"
+        # for object in hotels:
+        #     print("Hotel:", object.nom, object.estrelles)
         
-        while op not in "Ss":
-            mostrar_menu()
-            op = input("Introdueix una de les opcions del menú: ")
-            if op == "1":
-                mostrar_hotels(hotels)
-            elif op == "2":
-                mostrar_hotels(ordenar_per_estrelles(hotels))
-            elif op == "3":
-                buscar_hotels(hotels)
-            elif op == "4":
-                try:
-                    latitud = float(input("Intorudeix el valor de la latitud: "))
-                    longitud = float(input("Introdueix el valor de la longitud: "))
-                except ValueError:
-                    print("Error: latitud i longitud han de ser valors reals")
-                else:
-                    closest_hotel = hotel_mes_proper(hotels, latitud, longitud)
-                    print(f"L'hotel  més  proper  és  el  {closest_hotel}  a  {closest_hotel.distancia()} kms")
+        llista_hola = sorted(hotels, key=lambda x: x.nom)
+        for i in llista_hola:
+            print(i.nom)        
+        # omplir_llista_barris(barris, districtes)
+        # op = "a"
+        
+        # while op not in "Ss":
+        #     mostrar_menu()
+        #     op = input("Introdueix una de les opcions del menú: ")
+        #     if op == "1":
+        #         mostrar_hotels(hotels)
+        #     elif op == "2":
+        #         mostrar_hotels(ordenar_per_estrelles(hotels))
+        #     elif op == "3":
+        #         buscar_hotels(hotels)
+        #     elif op == "4":
+        #         try:
+        #             latitud = float(input("Intorudeix el valor de la latitud: "))
+        #             longitud = float(input("Introdueix el valor de la longitud: "))
+        #         except ValueError:
+        #             print("Error: latitud i longitud han de ser valors reals")
+        #         else:
+        #             closest_hotel = hotel_mes_proper(hotels, latitud, longitud)
+        #             print(f"L'hotel  més  proper  és  el  {closest_hotel}  a  {closest_hotel.distancia()} kms")
 
-            elif op in "Ss":
-                print("Sortint del programa")
-            else:
-                print("Opció no permesa")
-                time.sleep(0.5)
+        #     elif op in "Ss":
+        #         print("Sortint del programa")
+        #     else:
+        #         print("Opció no permesa")
+        #         time.sleep(0.5)
     finally:
         print("© Bernat Vidal i Joan Colillas")
     
--- part3.py
+++ part3.py
@@ -1,2 +1,11 @@
-if __main__ == __name__:
+def ordenar_per_nom(l_hotels):
+    return list(sorted(l_hotels, key=lambda x: x.nom))
+
+def carrers_amb_hotels(l_hotels):
+    return list(sorted(set(object.carrer for object in l_hotels)))
+
+def estrelles_per_barri(l_hotels, dict_barris):
+    pass
+
+def densitat_per_districte(l_hotels, dict_barris, dict_districtes):
     pass
